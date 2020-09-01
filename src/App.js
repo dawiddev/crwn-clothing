cc71c3c3e5dc10d1da5e3dece9eee11079552141
David Manor <david@dolphextechnologies.com>
2020-09-01 06:57:27 +0100

    Set up sign up component, Set up email and password sign up, set up user storage in firbase db

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Homepage from "./pages/homepage/homepage.component.jsx";
 import Shop from './pages/shop/shop.component.jsx'
 import Header from './components/header/header.component'
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from "./firebase/firbase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firbase.utils";
 
 
 class App extends React.Component{
@@ -21,10 +21,21 @@ class App extends React.Component{
   unsubscribeFromAuth = null
 
   componentDidMount(){
-   this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
+   this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+     if(userAuth){
+       const userRef = await createUserProfileDocument(userAuth)
 
-      console.log(user);
+       userRef.onSnapshot(snapshot =>{
+         this.setState({
+           currentUser: {
+             id: snapshot.id,
+             ...snapshot.data()
+           }
+         })
+       })
+     }else{
+       this.setState({currentUser: userAuth})
+     }
     })
   }
   
